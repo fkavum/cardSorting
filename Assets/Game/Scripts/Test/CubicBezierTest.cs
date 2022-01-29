a939d933e6a72364207bbafa4771a3bfba75aba6
DESKTOP-U5GJ455\fatih <fatihkavum@gmail.com>
2022-01-29 21:54:05 +0300

    Curve algorithm implemented. With test scene.

---
+++ Assets/Game/Scripts/Test/CubicBezierTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using CardSorting.Utils;
+using UnityEngine;
+
+public class CubicBezierTest : MonoBehaviour
+{
+    public GameObject objectPrefab;
+    public bool isNeedRecalculate = false;
+
+
+    public Vector2 startPoint = new Vector2(0, 0);
+    public Vector2 endPoint = new Vector2(5, 0);
+    public Vector2 controlPoint = new Vector2(2.5f, 2.5f);
+
+    public int segmentCount = 10;
+
+    private List<GameObject> _objects;
+
+    private void Update()
+    {
+        if (!isNeedRecalculate)
+        {
+            return;
+        }
+
+        isNeedRecalculate = false;
+
+        if (_objects != null)
+        {
+            foreach (var gameObject in _objects)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        _objects = new List<GameObject>();
+
+        CubicBezier spline = new CubicBezier(startPoint, endPoint, controlPoint);
+
+        for (int i = 0; i <= segmentCount; i++)
+        {
+            float t = i / (float)segmentCount;
+            Vector2 pos = spline.Solve(t);
+            float tan = spline.SolveTangent(t);
+            GameObject go = Instantiate(objectPrefab);
+            go.transform.position = pos;
+            go.transform.localRotation = Quaternion.Euler(new Vector3(0,0,tan));
+            go.name = $"createdObj";
+            _objects.Add(go);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Utils/CubicBezier.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace CardSorting.Utils
+{
+    public class CubicBezier
+    {
+        public Vector2 start;
+        public Vector2 end;
+        public Vector2 controlPoint;
+
+        public CubicBezier(Vector2 start, Vector2 end, Vector2 controlPoint)
+        {
+            this.start = start;
+            this.end = end;
+            this.controlPoint = controlPoint;
+        }
+
+        /// <summary>
+        /// Source: https://stackoverflow.com/questions/48602141/drawing-a-parabolic-arc-between-two-points-based-on-a-known-projectile-angle
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public Vector2 Solve(float t)
+        {
+            float a = (1 - t) * (1 - t);
+            float b = 2 * (1 - t) * t;
+            float c = t * t;
+
+            float x = a * start.x + b * controlPoint.x + c * end.x;
+            float y = a * start.y + b * controlPoint.y + c * end.y;
+            return new Vector2(x, y);
+        }
+
+        public float SolveTangent(float t)
+        {
+            // float a = 2*(1-t) -1;
+            // float b = 2 - 4*t;
+            // float c = 2*t;
+
+            float a = 2 * t - 2;
+            float b = 2 * (1 - 2 * t);
+            float c = 2 * t;
+
+            float x = a * start.x + b * controlPoint.x + c * end.x;
+            float y = a * start.y + b * controlPoint.y + c * end.y;
+            return Mathf.Atan(y / x) * Mathf.Rad2Deg;
+        }
+    }
+}
